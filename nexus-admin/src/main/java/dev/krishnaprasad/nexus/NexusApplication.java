4ea938c92f7ba3faf61f6e4c222d25f6c43aa2d0
me-krishnaprasad-dev <me.krishnaprasad.dev@gmail.com>
2026-04-03 11:19:46 +0530

    doc: Added Java Doc for Main Application class

--- nexus-admin/src/main/java/dev/krishnaprasad/nexus/NexusApplication.java
+++ nexus-admin/src/main/java/dev/krishnaprasad/nexus/NexusApplication.java
@@ -3,6 +3,13 @@ package dev.krishnaprasad.nexus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class will be the main entry point for the Nexus application. It will be responsible for starting the Spring Boot application and initializing any necessary components or configurations.
+ * The @SpringBootApplication annotation is a convenience annotation that adds all of the following:
+ * - @Configuration: Indicates that the class can be used by the Spring IoC container as a source of bean definitions.
+ * - @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings.
+ * - @ComponentScan: Tells Spring to look for other components, configurations, and services in the specified package, allowing it to find the controllers and other components that we will define in our application.
+ */
 @SpringBootApplication
 public class NexusApplication {
 
